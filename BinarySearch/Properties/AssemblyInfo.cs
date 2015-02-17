b656c0571ba01e2bcc8c9b223cb9ff1c9ccbd2bc
Martin Evans <martindevans@gmail.com>
2015-02-17 01:45:00 +0000

    Lowered target framework from 4.5 to 3.5 client
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- BinarySearch/Properties/AssemblyInfo.cs
+++ BinarySearch/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinarySearch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Performs binary searches (not necessarily on arrays)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BinarySearch")]
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
